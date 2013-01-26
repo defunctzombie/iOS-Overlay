51069192e9006e296f7e693a0260f32cdae9cac2
Roman Shtylman <shtylman@gmail.com>
2013-01-25 20:29:29 -0500

    make into a reusable module
    - add interactive support for example
    - add package.json

---
+++ example/index.js
@@ -0,0 +1,27 @@
+
+// this code should not be bundles
+// any requires it uses on the other hand should be bundled
+// actually, bundle this code but don't auto-run
+// that way we have the requires
+
+var overlay = require('ios-overlay');
+
+var button = document.createElement('button');
+button.innerHTML = 'Loading';
+
+button.addEventListener('click', function() {
+    overlay({ text: 'foo', duration: 2000});
+});
+
+button
+// =>
+
+var button = document.createElement('button');
+button.innerHTML = 'Success';
+
+button.addEventListener('click', function() {
+    overlay({ text: 'Success', icon: 'img/check.png', duration: 2000});
+});
+
+button
+// =>
